8c6ce38f7c4cb84470bdd69f0c61e9afca67f195
johnnychhsu <johnny.chhsu01@gmail.com>
2019-03-18 19:14:13 +0800

    Add wiki server in written in Go lang

---
+++ wiki.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"errors"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"regexp"
+)
+
+var templates = template.Must(template.ParseFiles("templates/edit.html", "emplates/view.html"))
+
+// It will panic if the expression compile fails
+var validPath = regexp.MustCompile("^/(edit|view|save)/([a-zA-Z0-9]+)$")
+
+type Page struct {
+	// The body element is a byte slice because that is the type expexted by the io library
+	Title string
+	Body  []byte
+}
+
+func getTitle(w http.ResponseWriter, p *http.Request) (string, error) {
+	m := validPath.FindStringSubmatch(r.URL.Path)
+	if m == nil {
+		http.NotFound(w, p)
+		return "", errors.New("Invalid Page Title")
+	}
+	return m[2], nil
+}
+
+// The third argument indicate that the file can be read and write by the current user
+func (p *Page) save() error {
+	// The string should be surround with "", not ''.
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	// ReadFile return file and error
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	// a panic would occur when pass a non-nil error. Otherwise it would return a *Template
+	err := templates.ExecuteTemplate(w, tmpl+".html", p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title, err := getTitle(w, r)
+	if err != nil {
+		return
+	}
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+	renderTemplate(w, "templates/view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title, err := getTitle(w, r)
+	if err != nil {
+		return
+	}
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	// ParseFile will return a *template.Template
+	// The printf in .html will treat body as string
+	renderTemplate(w, "templates/edit", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title, err := getTitle(w, r)
+	if err != nil {
+		return
+	}
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	// error could happen when saving files
+	err = p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+func main() {
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
